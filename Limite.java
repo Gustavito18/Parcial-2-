51ee7bc24dac73d80b3dc8281bc6e9d04719e3db
Gustavito18 <lmayorgao@miumg.edu.gt>
2022-05-01 20:57:03 -0600

    Limite

---
+++ Limite.java
@@ -0,0 +1,13 @@
+
+package tarjetasbancarias;
+
+
+public class Limite {
+    
+    int LimiteCredito = 10000;
+    
+    public int ClaseLimite (int valor){
+        
+        return LimiteCredito;
+    }
+}
