25d11fa8e8c75297c67341b61e25c5f62e714661
Gustavito18 <lmayorgao@miumg.edu.gt>
2022-05-01 21:09:28 -0600

    Metodo comprar tipo dos

---
+++ MetodoComprarDos.java
@@ -0,0 +1,18 @@
+
+package tarjetasbancarias;
+
+
+public class MetodoComprarDos {
+    
+    double CreditoComprarDos = 5000;
+    double SacarDos;
+    double solucion;
+    
+     public double ClaseComprarDos (double SacarDos){
+        
+        solucion = CreditoComprarDos - SacarDos;
+        
+        return solucion;
+    }
+     
+}
